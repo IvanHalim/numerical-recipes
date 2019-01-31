8fe8c7b6c7e51359853fc3c348b16d3041411388
ivanhalim <ivanhalim@users.noreply.github.com>
2019-01-31 14:38:19 -0800

    Create adalinesgd.py

---
+++ linear classifier/Stochastic Adaline/adalinesgd.py
@@ -0,0 +1,59 @@
+import numpy as np
+from numpy.random import seed
+
+class AdalineSGD:
+    def __init__(self, eta = 0.01, epoch = 10, shuffle = True, random_state = None):
+        self.eta = eta
+        self.epoch = epoch
+        self.w_initialized = False
+        self.shuffle = shuffle
+        if random_state:
+            seed(random_state)
+
+    def fit(self, X, y):
+        self._initialize_weights(X.shape[1])
+        self.cost_ = []
+        for _ in range(self.epoch):
+            if self.shuffle:
+                X, y = self._shuffle(X, y)
+            cost = 0.0
+            for xi, target in zip(X, y):
+                cost += self._update_weights(xi, target))
+            avg_cost = cost/len(y)
+            self.cost_.append(avg_cost)
+        return self
+
+    def partial_fit(self, X, y):
+        if not self.w_initialized:
+            self._initialize_weights(X.shape[1])
+        if y.ravel().shape[0] > 1:
+            for xi, target in zip(X, y):
+                self._update_weights(xi, target)
+        else:
+            self._update_weights(X, y)
+        return self
+
+    def _initialize_weights(self, m):
+        self.w_ = np.zeros(1 + m)
+        self.w_initialized = True
+
+    def _shuffle(self, X, y):
+        r = np.random.permutation(len(y))
+        return X[r], y[r]
+
+    def _update_weights(self, xi, target):
+        output = self.net_input(xi)
+        error  = target - output
+        self.w_[1:] += self.eta * xi.dot(error)
+        self.w_[0]  += self.eta * error
+        cost = 0.5 * error**2
+        return cost
+
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def activation(self, X):
+        return self.net_input(X)
+
+    def predict(self, X):
+        return np.where(self.activation(X) >= 0.0, 1, -1)
