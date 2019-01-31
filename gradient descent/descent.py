2efeb7e4be74d17fd207e918d66b15b6520d2170
ivanhalim <ivanhalim@users.noreply.github.com>
2019-01-30 21:52:56 -0800

    Update descent.py

--- gradient descent/descent.py
+++ gradient descent/descent.py
@@ -6,10 +6,10 @@
 # Given a starting value x, the algorithm will follow the
 # gradient at x until a local minimum value is found.
 #
-# We use gradient descent rather than direct calculation
-# because it is computationally cheaper for large amount
-# of data and because it is a more general method. Also,
-# some problems simply don't have a predefined formula.
+# Gradient descent is an iterative method to find the local
+# minimum of a function. We use an iterative method rather
+# than the closed form solution because it's computationally
+# cheaper.
 
 def gradient_descent(function, cur_x, epoch, precision):
     gamma              = 0.01  # step size multiplier (learning rate)
@@ -26,12 +26,12 @@ if __name__ == '__main__':
 
     # f'(x) = 4x^3 - 9x^2
     df = lambda x: 4 * x**3 - 9 * x**2
-    
+
     # The algorithm starts at x = 6
     # Iterate 10000 times
     # With a precision of 0.00001
     x = gradient_descent(df, 6, 10000, 0.00001)
-    
+
     print("The local minimum occurs at", x)
     # The output for the above will be:
     # 'The local minimum occurs at 2.2499646074278457'
