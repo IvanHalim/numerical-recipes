bfc1d0ff26d1fc925fe7662d4188ddff70312627
ivanhalim <ivanhalim@users.noreply.github.com>
2019-01-31 10:12:33 -0800

    Update adaline.py

--- linear classifier/Adaptive Linear Neuron/adaline.py
+++ linear classifier/Adaptive Linear Neuron/adaline.py
@@ -18,18 +18,20 @@ class AdalineGD:
         self.weights = np.zeros(1 + X.shape[1])
         self.costs   = []
 
-        N = len(y)
         for _ in range(self.epoch):
-            output = self.net_input(X)
+            output = self.activation(X)
             errors = y - output
-            self.weights[1:] += (2/float(N)) * self.eta * X.T.dot(errors)
-            self.weights[0]  += (2/float(N)) * self.eta * errors.sum()
-            cost = (errors**2).sum() / float(N)
+            self.weights[1:] += self.eta * X.T.dot(errors)
+            self.weights[0]  += self.eta * errors.sum()
+            cost = (errors**2).sum() / 2.0
             self.costs.append(cost)
         return self
 
     def net_input(self, X):
         return np.dot(X, self.weights[1:]) + self.weights[0]
 
+    def activation(self, X):
+        return self.net_input(X)
+
     def predict(self, X):
-        return np.where(self.net_input(X) >= 0.0, 1, -1)
+        return np.where(self.activation(X) >= 0.0, 1, -1)
